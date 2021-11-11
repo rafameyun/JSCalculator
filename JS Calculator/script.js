3e9880825a654c0c0e0779598a6d4a1b5caff290
Rafael Medeiros <78233061+rafameyun@users.noreply.github.com>
2021-11-11 11:36:41 -0300

    Add files via upload
    Git doesn't work on my pc :( but imagine that have the commands
    git add change4.html
    git add script.js
    git add style.css
    git commit -m Add files via upload
    git push

---
+++ JS Calculator/script.js
@@ -0,0 +1,11 @@
+function display(val){
+    document.getElementById("textval").value+=val
+}
+function result(){
+    let x = document.getElementById("textval").value
+    let y = eval(x)
+    document.getElementById("textval").value = y
+}
+function clr(){
+    document.getElementById("textval").value = ""
+}
